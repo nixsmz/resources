aa6c780efda7c0d71d865f0f03fd6c1ec10d8969
Nix Smz <nixcrownzz@gmail.com>
2023-04-01 03:19:36 +0200

    @nix

--- webradios/sort.py
+++ webradios/sort.py
@@ -1,7 +1,10 @@
-import json
+import json, sys
 
-file = open("stations.json")
+if len(sys.argv) != 2:
+    print("JSON file missing.")
+    exit(0)
+file = open(sys.argv[1])
 data = json.load(file)
 file.close()
-order = sorted(sorted(data, key=lambda x: x['name']), key=lambda y: y['country'])
+order = sorted(sorted(data, key=lambda x: x['name'].lower()), key=lambda y: y['country'].lower())
 print(json.dumps(order, indent=4))
